faf2f119dd50681155ebafedbe3e9e83f1191d21
Diego Quispe <diego.quispe.ap@utec.edu.pe>
2024-07-01 23:57:43 -0500

    ✨ Add frontend svgs

--- src/components/Ability.jsx
+++ src/components/Ability.jsx
@@ -1,3 +1,13 @@
-export default function Ability() {
-  return (<></>);
+export default function Ability({side, svgs}) {
+  return (
+    <section className="box">
+      <h3>{side}</h3>
+      <div className="svgs">
+        {svgs.map((svg, index) => (
+          <img key={index} src={svg} alt={side}/>
+        ))}
+      </div>
+
+    </section>
+  );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,21 @@
 import Experience from "./Experience.jsx";
 import Ability from "./Ability.jsx";
 import Project from "./Project.jsx";
+import cssSvg from "../assets/css.svg";
+import htmlSvg from "../assets/html.svg";
+import jsSvg from "../assets/javascript.svg";
+import reactSvg from "../assets/react.svg";
 
 export default function Main(){
+  let frontSvgs = [htmlSvg, cssSvg, jsSvg, reactSvg];
+
   return(
     <>
       <main>
         <Experience/>
-        <section id="abilities" className="box">
-          Abilities
-          <Ability/>
+        <section id="abilities">
+          <Ability side="Frontend" svgs={frontSvgs}/>
+          {/*<Ability side="Backend"/>*/}
         </section>
         <Project id="project1"/>
         <Project id="project2"/>

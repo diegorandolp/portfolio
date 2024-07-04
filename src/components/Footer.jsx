e18b73c698929afd1c6058c120730e892b6c97ce
Diego Quispe <diego.quispe.ap@utec.edu.pe>
2024-07-03 23:55:24 -0500

    ✨ Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 export default function Footer(){
   return(
     <footer className="box">
-
+        <a href="https://www.linkedin.com/in/diegorandolp/" target="_blank" rel="noreferrer">Linkedin</a>
+        <a href="https://www.linkedin.com/in/diegorandolp/" target="_blank" rel="noreferrer">Github</a>
     </footer>
   );
 }
